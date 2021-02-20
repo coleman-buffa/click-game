ddaf126962829d67ae782e8a3306871151ae0a77
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-20 12:05:55 -0800

    Initialize project directory.

---
+++ click-game/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Wrapper>
+    {/* Site components go here */}
+    </Wrapper>
+  );
+}
+
+export default App;
